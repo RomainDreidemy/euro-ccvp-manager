2a904ae4d22d8be23cb8eb025699765545dafdf5
kryweb <dreidemyromain@gmail.com>
2019-11-25 18:30:21 +0100

    Ajout de la route pour créer l'utilisateur alain et moi

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -29,4 +29,32 @@ class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', []);
     }
+
+    /**
+     * @Route("/user/create/alain", name="userCreateAlain")
+     */
+    public function userCreateAlain(EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $password = $passwordEncoder->encodePassword($user, 'Gwada2016#');
+
+        $user->setEmail('contact@euroccvp.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($password);
+
+        $em->persist($user);
+
+        $user = new User();
+        $password = $passwordEncoder->encodePassword($user, 'Etxzh93r');
+
+        $user->setEmail('dreidemyromain@gmail.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($password);
+
+        $em->persist($user);
+
+        $em->flush();
+
+        return $this->render('home/index.html.twig', []);
+    }
 }
